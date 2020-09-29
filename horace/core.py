d749c52b9249d78937b4cbe437ad7f72623491a5
omare13 <darknessit@msn.com>
2020-09-29 15:20:44 +0200

    Test con RDFLib

--- horace/core.py
+++ horace/core.py
@@ -1,110 +1,174 @@
 """Core module."""
-import io
-import uuid
-from typing import List
-
-from owlready2 import get_ontology
-from owlready2 import Ontology
-from rdflib import Graph
-
-
-ONTOLOGY_URL_TEMPLATE = ("https://raw.githubusercontent.com/linhd-postdata/"
-                         "{onto}-ontology/master/postdata-{onto}.owl")
-ONTOLOGIES = {
-    "core":       ONTOLOGY_URL_TEMPLATE.format(onto="core"),
-    "structural": ONTOLOGY_URL_TEMPLATE.format(onto="structuralElements"),
-    "prosodic":   ONTOLOGY_URL_TEMPLATE.format(onto="prosodicElements"),
-    "literary":   ONTOLOGY_URL_TEMPLATE.format(onto="literaryAnalysis"),
-}
+import os
+import json
+from rdflib import Graph, Namespace, RDF, RDFS, Literal, XSD, FOAF, DC
+from horace import uri
+import re
+from rdflib.plugins.stores import sparqlstore
 
 
-def get_scansion_graph(scansion: dict) -> Graph:
-    """Transform a scansion dictionary into a compliant LOD graph.
-    :param scansion: Dictionary with a Rantanplan-like scansion analysis
-    :return: RDFLib Graph with the individuals from scansion
-    """
-    structural_onto = get_ontology(ONTOLOGIES["structural"]).load()
-    onto = add_structural_individuals(scansion, structural_onto)
-    return onto_to_graph(onto)
+CORE = Namespace("http://postdata.linhd.uned.es/ontology/postdata-core#")
+STRUCT = Namespace("http://postdata.linhd.uned.es/ontology/postdata-structuralElements#")
+
+NAMESPACES = {"xsd": "http://www.w3.org/2001/XMLSchema#",
+              "objectrole": "http://www.ontologydesignpatterns.org/cp/owl/objectrole.owl#",
+              "obj": "http://www.openrdf.org/rdf/2009/object#",
+              "skos": "http://www.w3.org/2004/02/skos/core#",
+              "pdstruct": "http://postdata.linhd.uned.es/ontology/postdata-structuralElements#",
+              "pdcore": "http://postdata.linhd.uned.es/ontology/postdata-core#",
+              "pd": "http://postdata.linhd.uned.es/resource/",
+              "dc": "http://purl.org/dc/elements/1.1/",
+              "foaf": "http://xmlns.com/foaf/0.1/"}
 
 
-def onto_to_graph(onto: Ontology) -> Graph:
-    """Convert an OWLReady2 ontology graph to RDFLib.
-    :param onto: OWLReady2 ontology
-    :return: RDFLib Graph
-    """
-    with io.BytesIO() as file:
-        onto.save(file=file, format="rdfxml", filter=filter_individuals)
-        rdf = file.getvalue().decode("utf8")
-        return Graph().parse(data=rdf)
+def to_rdf(_json) -> Graph:
+    g = Graph()
+
+    g = g + add_core_elements(_json, "PLCSO")
+    g = g + add_structural_elements(_json, "PLCSO")
+
+    return g
 
 
-def filter_individuals(graph: Graph, s: int, *args) -> bool:
-    """Filtering function to keep just the individuals of the classes
-    we are interested in when serializing.
-    :param graph: OWLReady2 graph
-    :param s: storeid specifying an individual in graph.onto
-    :return: Boolean deciding whether the instance specified by its storeid
-             should be returned or not
-    """
-    classes = [graph.onto.Line, graph.onto.Word, graph.onto.Syllable]
-    return s >= 0 and any(isinstance(graph.onto.world._get_by_storid(s), cls)
-                          for cls in classes)
+def add_core_elements(_json, dataset):
+    g = Graph()
+    poem_title = _json["poem_title"]
+    author = _json["author"]
+
+    r_redaction = uri.uri_redaction(author, poem_title, dataset)
+    r_creator = uri.uri_creator_role(author, poem_title)
+    r_poetic_work = uri.uri_poetic_work(author, poem_title)
+    r_person = uri.uri_person(author)
+
+    # Types
+    g.add((r_poetic_work, RDF.type, CORE.PoeticWork))
+    g.add((r_redaction, RDF.type, CORE.Redaction))
+    g.add((r_creator, RDF.type, CORE.CreatorRole))
+    g.add((r_person, RDF.type, CORE.Person))
+
+    # Labels
+    g.add((r_poetic_work, DC.title, Literal(poem_title, lang="es")))
+    g.add((r_person, FOAF.name, Literal(author, lang="es")))
+
+    # Object Properties
+    g.add((r_poetic_work, CORE.isRealisedThrough, r_redaction))
+    g.add((r_redaction, CORE.hasCreator, r_creator))
+    g.add((r_creator, CORE.isRoleOf, r_person))
+
+    return g
 
 
-def add_structural_individuals(scansion: dict, onto: Ontology) -> Ontology:
-    """Transform a Rantanplan scansion dictionary into an OWLReady ontology.
-    This function performs all changes directly on onto.
-    :param scansion: Dictionary with a Rantanplan-like scansion analysis
-    :param onto: OWLReady2 ontology
-    :return: The modified ontology onto
-    """
-    for line in scansion:
-        if "tokens" not in line:
-            continue
-        o_line = onto.Line(
-            f"{onto.Line.name}/{uuid.uuid4()}",
-            content=[join_tokens(line["tokens"])],
-        )
-        o_line.has_words = []
-        for token in line["tokens"]:
-            o_word = onto.Word(
-                f"{onto.Word.name}/{uuid.uuid4()}",
-                content=[join_syllables(token)],
-                belongsToLine=[o_line],
-            )
-            o_word.has_syllables = []
-            if "word" not in token:
-                continue
-            for syllable in token["word"]:
-                o_syllable = onto.Syllable(
-                    f"{onto.Syllable.name}/{uuid.uuid4()}",
-                    content=[syllable["syllable"]],
-                    belongsToWord=[o_word],
-                )
-                o_word.has_syllables += [o_syllable]
-            o_line.has_words += [o_word]
-    return onto
+def add_structural_elements(_json, dataset):
+    g = Graph()
+    poem_title = _json["poem_title"]
+    author = _json["author"]
+
+    r_stanza_list = uri.uri_stanza_list(poem_title, author, dataset)
+    r_redaction = uri.uri_redaction(author, poem_title, dataset)
+
+    # Add stanza_list type
+    g.add((r_stanza_list, RDF.type, STRUCT.OrderedStanzaList))
+    # Add redaction to stanza_list
+    g.add((r_redaction, STRUCT.hasStanzaList, r_stanza_list))
+
+    for stanza in _json["stanzas"]:
+        stanza_number = stanza["stanza_number"]
+        stanza_text = stanza["stanza_text"]
+
+        r_stanza = uri.uri_stanza(stanza_number, author, poem_title, dataset)
+        # Add Stanza Type
+        g.add((r_stanza_list, RDF.type, STRUCT.Stanza))
+        # Add StanzaList to Stanza
+        g.add((r_stanza_list, STRUCT.hasStanzaItem, r_stanza))
+
+        # Add Stanza DP
+        g.add((r_stanza, STRUCT.content, Literal(stanza_text, lang="es")))
+        g.add((r_stanza, STRUCT.stanzaNumber, Literal(stanza_number, datatype=XSD.nonNegativeInteger)))
+
+        for line in stanza["lines"]:
+            line_number = line["line_number"]
+            line_text = line["line_text"]
+
+            r_line = uri.uri_line(line_number, stanza_number, author, poem_title, dataset)
+            # Add line type
+            g.add((r_line, RDF.type, STRUCT.Line))
+            # Add stanza to line
+            g.add((r_stanza, STRUCT.hasLine, r_line))
+            # Add line DP
+            g.add((r_line, STRUCT.content, Literal(line_text, lang="es")))
+            g.add((r_line, STRUCT.lineNumber, Literal(line_number, datatype=XSD.nonNegativeInteger)))
+
+            if "words" in line.keys():
+                for i, word in enumerate(line["words"]):
+                    word_number = str(i)
+                    word_text = word["word_text"]
+
+                    r_word = uri.uri_word(word_number, line_number, stanza_number, author, poem_title, dataset)
+                    # Add word type
+                    g.add((r_word, RDF.type, STRUCT.Word))
+                    # Add line to word
+                    g.add((r_line, STRUCT.hasContent, r_word))
+                    # Add Word DP
+                    g.add((r_word, STRUCT.content, Literal(word_text, lang="es")))
+                    g.add((r_word, STRUCT.lineNumber, Literal(word_number, datatype=XSD.nonNegativeInteger)))
+
+                    if "syllables" in word.keys():
+                        for i, syllable in enumerate(word["syllables"]):
+                            syllable_number = str(i)
+                            r_syllable = uri.uri_syllable(syllable_number, word_number, line_number, stanza_number, author, poem_title, dataset)
+                            # Add Syllable type
+                            g.add((r_syllable, RDF.type, STRUCT.Syllable))
+                            # Add Syllable DP
+                            g.add((r_syllable, STRUCT.positionInWord, Literal(syllable_number, datatype=XSD.nonNegativeInteger)))
+                            g.add((r_syllable, STRUCT.content, Literal(syllable, datatype=XSD.nonNegativeInteger)))
+    return g
 
 
-def join_tokens(tokens: List[str]) -> str:
-    """Join all words from a list of tokens into a string.
-    :param tokens: List of dictionaries representing tokens
-    :return: String of words
-    """
-    output = []
-    for token in tokens:
-        item = join_syllables(token)
-        output.append(item)
-    return " ".join(output)
+def sparql_update(graph: Graph):
+    import requests
+    from requests.auth import HTTPDigestAuth
+
+    prefixes = ""
+    for prefix, uri in NAMESPACES.items():
+        graph.namespace_manager.bind(prefix, uri)
+        prefixes = prefixes + "prefix " + prefix + ": " + "<" + uri + ">" + "\n"
+
+    data = graph.serialize(format='turtle').decode('utf-8')
+
+    url = "http://localhost:8890/sparql-auth"
+    query = """
+    INSERT IN GRAPH <http://localhost:8890/POSTDATA/>
+    {
+        #DATA#
+    }""".replace("#DATA#", data)
+
+    query = re.sub(r'@prefix.*>\s\.', "", query)
+    query = prefixes + query
+    # print(query)
+
+    auth = HTTPDigestAuth(username="dba", password="dba")
+    result = requests.post(url, params={"query": query}, auth=auth)
+    print(result.text)
+    return result.status_code
 
 
-def join_syllables(token: List[dict]) -> str:
-    """Join all symbols and syllables from a list of tokens into a string."
-    :param tokens: List of dictionaries representing tokens
-    :return: String of syllables
-    """
-    if "symbol" in token:
-        return token["symbol"]
-    else:
-        return "".join([syll["syllable"] for syll in token["word"]])
+total_jsons = {}
+base_root = "/home/omar/Documents/POSTDATA/Averell/datasets_json/"
+datasets = os.listdir(base_root)
+print(datasets)
+
+for dataset in datasets:
+    jsons_root = base_root + dataset + "/averell/parser"
+    authors = os.listdir(jsons_root)
+    for author in authors:
+        json_files = os.listdir(jsons_root + "/" + author)
+        for json_file in json_files:
+            if json_file[-5:] == ".json":
+                total_jsons.update({json_file[:-5]: jsons_root + "/" + author + "/" + json_file})
+
+n_doc = 0
+for name, root in total_jsons.items():
+    n_doc += 1
+    rdf = to_rdf(json.load(open(root)))
+    status = sparql_update(rdf)
+    print(name, root, n_doc, status)
